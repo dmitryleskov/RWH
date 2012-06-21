f78168d298b6801bf0ade81bd6f178a96aceea95
Dmitry Leskov <git@dmitryleskov.com>
2012-06-21 10:16:45 +0700

    Added comments, variations, notable solutions from book comments

--- exercises/ch03/04-palindrome.hs
+++ exercises/ch03/04-palindrome.hs
@@ -4,7 +4,7 @@
     [1,2,3], your function should return [1,2,3,3,2,1].
 --}
 
--- Solution based on the material of the first three chapters
+-- Solution that I thought is based on the material of the first three chapters
 p :: [a] -> [a]
 p [] = []
 p list = q [] list
@@ -14,6 +14,26 @@ p list = q [] list
           | otherwise = x : (q (x:t) xs) 
             -- prepend t with x, repeat for xs, and put x back in front                          
 
--- Obvious after Chapter 4, but what about performance?
+-- Then I read that the (++) operator was in fact introduced
+-- in Chapter 1 alongside (:)
 p1 :: [a] -> [a]
-p1 x = x ++ reverse x
+p1 [] = []
+p1 (x:xs) = x:p1 xs ++ [x]
+-- However, I have doubts about performance of p1 on long lists
+-- specifically about it appending single-element lists to tail
+
+-- Obvious after Chapter 4, but again what about performance?
+p2 :: [a] -> [a]
+p2 x = x ++ reverse x
+
+-- Notable solutions from comments at http://book.realworldhaskell.org/read/defining-types-streamlining-functions.html
+
+-- Looks clever but works incorrectly (yields [3,2,1,1,2,3]) by Beatrix
+palindrome xs = myReverser xs xs
+  where myReverser         [] result = result
+        myReverser (first:xs) result = myReverser xs (first : result)
+
+-- Claimed to be as fast as "x ++ reverse x" by Andrew Pritchard
+palindromize3 l = palindromize3' l []
+palindromize3' [] fs = fs
+palindromize3' (x:xs) fs = x:palindromize3' xs (x:fs)
