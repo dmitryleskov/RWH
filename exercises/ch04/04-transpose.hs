5d441a92ef5d618a8b49fb704f9d392f4e3bceff
Dmitry Leskov <git@dmitryleskov.com>
2012-08-02 09:50:26 +0700

    Implemented variants for all sets of restrictions

--- exercises/ch04/04-transpose.hs
+++ exercises/ch04/04-transpose.hs
@@ -1,4 +1,4 @@
-
+{-
 import System.Environment (getArgs)
 
 interactWith function inputFile outputFile = do
@@ -13,38 +13,67 @@ main = mainWith myFunction
             _ -> putStrLn "error: exactly two arguments needed"
                                                   
 myFunction = transpose
+-}
 
+-- The transpose operation cannot be defined for arbitrary texts 
+-- so that transpose (transpose text) == text
+
+-- The narrowest restriction is that the text must be "rectangular",
+-- i.e. all lines have the same length and the last line ends with
+-- the newline character.
+
+-- Using no "material from the future":
+transposeRect :: String -> String
+transposeRect text = unlines (tr (lines text))
+  where
+    tr []          = []
+    tr ((y:ys):[]) = [y] : tr [ys]
+    tr (x:xs)      = zipWith (:) x (tr xs)
+
+-- Using map:
+transposeRectMap :: String -> String
+transposeRectMap text = unlines (tr (lines text))
+  where
+    tr []     = []
+    tr (x:[]) = map (:[]) x
+    tr (x:xs) = zipWith (:) x (tr xs)
+
+-- Using repeat:
+transposeRectRepeat :: String -> String
+transposeRectRepeat text = unlines (tr (lines text))
+  where
+    tr []     = []
+    tr (x:[]) = zipWith (:) x (repeat [])
+    tr (x:xs) = zipWith (:) x (tr xs)
+
+-- The transpose openarion can also be defined in a completely
+-- reversible manner for 'convex' files, in which no line is longer
+-- than the preceding one and no trailing empty lines are present.
+
+transposeConvex :: String -> String
+transposeConvex text = unlines (tr (lines text))
+  where
+    tr []     = []
+    tr (x:xs) = zipWith (:) x (tr xs ++ repeat [])
+
+-- Finally, the least restrictive version can handle files
+-- in which lines have no trailing blanks and the last line 
+-- is not empty and ends with a newline character.
 
 transpose :: String -> String
-transpose text = unlines (scan (removeTrailingNewlines (lines text)) [])
-  where
-    removeTrailingNewlines lines = 
-      case lines of
-        []     -> []
-        (l:[]) -> if null l then [] else [l]
-        (l:ls) -> let ps = removeTrailingNewlines ls
-                   in if null ps && null l
-                      then []
-                      else l:ps
-
-    -- Builds a row from the heads of lines, appends it to rows and
-    -- calls itself for tails until there are no more tails
-    scan :: [String] -> [String] -> [String]
-    scan []    rows = rows
-    scan lines rows = let (row, tails) = getRow lines
-                       in scan tails (rows ++ [row])
-
-    -- Given a list of Strings, returns a tuple contaning a String
-    -- formed by their heads and a list of their tails. 
-    -- Any trailing empty tails are trimmed.
-    getRow :: [String] -> (String, [String])
-    getRow []           = ("", [])
-    getRow (line:lines) = let (h,t) = decapitate line
-                              (hs, ts) = getRow lines
-                           in (h : hs, if null t && null ts
-                                       then []
-                                       else t : ts) 
+transpose text = unlines (tr (lines text))
+  where 
+    tr [] = []
+    tr (x:xs) = zipNPad x (tr xs)
       where
-        decapitate line = if null line 
-                          then (' ', "") 
-                          else (head line, tail line)
+        -- zipNPad line lines is equivalent to zipWith (:) line lines
+        -- with either line padded with trailing blanks, or lines padded
+        -- with trailing empty Strings so that length line == length lines.
+        zipNPad :: String -> [String] -> [String]
+        zipNPad ""     []     = []
+        zipNPad (c:cs) []     = trimCat c   "" : zipNPad cs []
+        zipNPad ""     (l:ls) = trimCat ' ' l  : zipNPad "" ls
+        zipNPad (c:cs) (l:ls) = trimCat c   l  : zipNPad cs ls
+          where
+            -- Ensures that there are no trailing blanks
+            trimCat c l = if c == ' ' && null l then "" else c:l
